7dc88ae6e4a65b9891e49242daab125f0cf42491
sumitsingh2503 <sumit.s1010@gmail.com>
2021-11-09 12:30:08 +0530

    2nd commit

---
+++ src/pages/SearchPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class SearchPage {
+	
+	public void search()
+	{
+		System.out.println("Search Page");
+	}
+
+}
--- src/tests/LoginPageTest.java
+++ src/tests/LoginPageTest.java
@@ -5,6 +5,9 @@ public class LoginPageTest {
 	public void test1() {
 		
 		System.out.println("test 1-LP");
+		String un = "username";
+		String pass = "admin";
+		System.out.println("Logged in..");
 	}
 	
 }
